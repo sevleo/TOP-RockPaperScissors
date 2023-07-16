13877e78cd15fcad0c77bf2c99b52cafdd72d530
Seva <seva.leonov@hotmail.com>
2023-07-16 13:29:05 +0700

    Add begin match button

--- js.js
+++ js.js
@@ -21,47 +21,53 @@ function playRound (playerSelection='rock', getComputerChoice) {
 
     else if ((playerSelection.toLowerCase() === "rock" || getComputerChoice === "rock") && (playerSelection.toLowerCase() === "scissors" || getComputerChoice === "scissors")){
         if (playerSelection.toLowerCase() === "rock") {
-            // playerScore += 1;
+            playerScore += 1;
+            updateScore();
             return "Rock beats Scissors. You win!";
         }
         else
-            // computerScore += 1;
+            computerScore += 1;
+            updateScore();
             return "Rock beats Scissors. You lose!";
     }
 
     else if ((playerSelection.toLowerCase() === "rock" || getComputerChoice === "rock") && (playerSelection.toLowerCase() === "paper" || getComputerChoice === "paper")){
         if (playerSelection.toLowerCase() === "rock") {
-            // computerScore += 1;
+            computerScore += 1;
+            updateScore();
             return "Paper beats Rock. You lose!";
         }
         else
-            // playerScore += 1;
+            playerScore += 1;
+            updateScore();
             return "Paper beats Rock. You win!";
     }
 
     else if ((playerSelection.toLowerCase() === "paper" || getComputerChoice === "paper") && (playerSelection.toLowerCase() === "scissors" || getComputerChoice === "scissors")){
         if (playerSelection.toLowerCase() === "paper") {
-            // computerScore += 1;
+            computerScore += 1;
+            updateScore();
             return "Scissors beat Paper. You lose!";
         }
         else
-            // playerScore += 1;
+            playerScore += 1;
+            updateScore();
             return "Scissors beat Paper. You win!";
     }
     
     else
-        // playerScore -= 1; 
+        playerScore -= 1; 
+        updateScore();
         return "This move is illegal! Taking one point from the player."
 }
 
-// let results = document.getElementsByClassName('results');
 let resultsDiv = document.querySelector('.results');
 let playerChoiceDiv = document.querySelector('.player-choice');
 let computerChoiceDiv = document.querySelector('.computer-choice');
-// const para = document.createTextNode("test");
 
+const weaponDiv = document.querySelector('div.weapon');
 
-const btns = Array.from(document.querySelectorAll('button'));
+const btns = Array.from(weaponDiv.querySelectorAll('button'));
 btns.forEach(btn => btn.addEventListener("click", handleClick));
 
 function handleClick(e) {
@@ -76,5 +82,22 @@ function handleClick(e) {
 }
 
 
+playerScore = 0;
+computerScore = 0;
 
+let scorePlayerNode = document.querySelector('.score-player');
+let scoreComputerNode = document.querySelector('.score-computer');
+let matchResultNode = document.querySelector('.match-result');
+
+function updateScore() {
+    if (playerScore == 5 ) {
+        matchResultNode.textContent = "You win!";
+    }
+
+    else if (computerScore == 5) {
+        matchResultNode.textContent = "Computer wins!"
+    }
+    scorePlayerNode.textContent = `Player score: ${playerScore}`;
+    scoreComputerNode.textContent = `Computer score: ${computerScore}`;
+}
 
