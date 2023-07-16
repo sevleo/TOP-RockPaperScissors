9240105f0e45ed94dc0fc6368d85f687389b4432
Seva <seva.leonov@hotmail.com>
2023-07-16 12:58:44 +0700

    Added UI

--- js.js
+++ js.js
@@ -12,9 +12,7 @@ function getComputerChoice () {
 }
 
 
-function playRound (playerSelection='rock', getComputerChoice) {
-    console.log(playerSelection);
-    console.log(getComputerChoice);
+function playRound (playerSelection='rock', getComputerChoice) {    
 
     if (playerSelection.toLowerCase() === getComputerChoice) {
         
@@ -23,56 +21,60 @@ function playRound (playerSelection='rock', getComputerChoice) {
 
     else if ((playerSelection.toLowerCase() === "rock" || getComputerChoice === "rock") && (playerSelection.toLowerCase() === "scissors" || getComputerChoice === "scissors")){
         if (playerSelection.toLowerCase() === "rock") {
-            playerScore += 1;
+            // playerScore += 1;
             return "Rock beats Scissors. You win!";
         }
         else
-            computerScore += 1;
+            // computerScore += 1;
             return "Rock beats Scissors. You lose!";
     }
 
     else if ((playerSelection.toLowerCase() === "rock" || getComputerChoice === "rock") && (playerSelection.toLowerCase() === "paper" || getComputerChoice === "paper")){
         if (playerSelection.toLowerCase() === "rock") {
-            computerScore += 1;
+            // computerScore += 1;
             return "Paper beats Rock. You lose!";
         }
         else
-            playerScore += 1;
+            // playerScore += 1;
             return "Paper beats Rock. You win!";
     }
 
     else if ((playerSelection.toLowerCase() === "paper" || getComputerChoice === "paper") && (playerSelection.toLowerCase() === "scissors" || getComputerChoice === "scissors")){
         if (playerSelection.toLowerCase() === "paper") {
-            computerScore += 1;
+            // computerScore += 1;
             return "Scissors beat Paper. You lose!";
         }
         else
-            playerScore += 1;
+            // playerScore += 1;
             return "Scissors beat Paper. You win!";
     }
     
     else
-        playerScore -= 1; 
+        // playerScore -= 1; 
         return "This move is illegal! Taking one point from the player."
 }
 
+// let results = document.getElementsByClassName('results');
+let resultsDiv = document.querySelector('.results');
+let playerChoiceDiv = document.querySelector('.player-choice');
+let computerChoiceDiv = document.querySelector('.computer-choice');
+// const para = document.createTextNode("test");
 
-function game() {
-    playerScore = 0;
-    computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-    console.log(`The player's score is ${playerScore}. The computer's score is ${computerScore}. `)
-    if (playerScore > computerScore) {
-        console.log("Player is the winner!")
-    }
-    else if (computerScore > playerScore) {
-        console.log("Computer is the winner!")
-    }
-    else console.log("Player and computer have a tie!")
+
+const btns = Array.from(document.querySelectorAll('button'));
+btns.forEach(btn => btn.addEventListener("click", handleClick));
+
+function handleClick(e) {
+    let computerChoice = getComputerChoice();
+    let playerChoice = e.target.id;
+
+
+    playerChoiceDiv.textContent = `The player chose ${playerChoice} as a weapon!`;
+    computerChoiceDiv.textContent = `The computer counters with ${computerChoice}.`;
+    resultsDiv.textContent = playRound(playerChoice, computerChoice);
+
 }
 
-game();
+
+
+
