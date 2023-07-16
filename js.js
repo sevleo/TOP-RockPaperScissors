4125cddcc366769dd74bc3dc4a41f51175abc49e
Seva <seva.leonov@hotmail.com>
2023-07-16 15:35:48 +0700

    Add dynamic DOM

--- js.js
+++ js.js
@@ -1,3 +1,80 @@
+let beginBtn = document.querySelector('#game-start');
+beginBtn.addEventListener("click", beginMatch);
+
+playerScore = 0;
+computerScore = 0;
+
+const scorePlayerNode = document.createElement("div");
+scorePlayerNode.classList.add("score-player");
+scorePlayerNode.textContent = `Your score is 0.`;
+
+const scoreComputerNode = document.createElement("div");
+scoreComputerNode.classList.add("score-computer");
+scoreComputerNode.textContent = `Computer score is 0.`;
+
+function beginMatch() {
+    const weaponDiv = document.createElement("div");
+    weaponDiv.classList.add("weapon");
+    document.body.append(weaponDiv);
+    const weaponDivText = document.createElement("div");
+    weaponDivText.textContent = "Choose your weapon";
+    weaponDivText.classList.add("weapon-div-text");
+    weaponDiv.append(weaponDivText);
+    beginBtn.remove();
+
+    const weapons = ["Rock", "Paper", "Scissors"];
+
+    weapons.forEach((weapon) => {
+        const btn = document.createElement("button");
+        btn.id = weapon.toLocaleLowerCase();
+        btn.textContent = weapon;
+        weaponDiv.append(btn);
+        btn.addEventListener("click", handleClick)
+    })
+
+
+    document.body.append(scorePlayerNode);
+
+    document.body.append(scoreComputerNode);
+}
+
+
+const playerChoiceDiv = document.createElement("div");
+playerChoiceDiv.classList.add("player-choice");
+const computerChoiceDiv = document.createElement("div");
+computerChoiceDiv.classList.add("computer-choice");
+const resultsDiv = document.createElement("div");
+resultsDiv.classList.add("results");
+
+function handleClick(e) {
+    let computerChoice = getComputerChoice();
+    let playerChoice = e.target.id;
+
+    document.body.append(playerChoiceDiv);    
+    document.body.append(computerChoiceDiv);
+    document.body.append(resultsDiv);
+
+    playerChoiceDiv.textContent = `The player chose ${playerChoice} as a weapon!`;
+    computerChoiceDiv.textContent = `The computer counters with ${computerChoice}.`;
+    resultsDiv.textContent = playRound(playerChoice, computerChoice);
+
+}
+
+
+function updateScore() {
+    if (playerScore == 5 ) {
+        resultsDiv.textContent = "You win!";
+    }
+
+    else if (computerScore == 5) {
+        resultsDiv.textContent = "Computer wins!"
+    }
+    scorePlayerNode.textContent = `Player score: ${playerScore}`;
+    scoreComputerNode.textContent = `Computer score: ${computerScore}`;
+}
+
+
+
 function getComputerChoice () {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
     
@@ -60,44 +137,3 @@ function playRound (playerSelection='rock', getComputerChoice) {
         updateScore();
         return "This move is illegal! Taking one point from the player."
 }
-
-let resultsDiv = document.querySelector('.results');
-let playerChoiceDiv = document.querySelector('.player-choice');
-let computerChoiceDiv = document.querySelector('.computer-choice');
-
-const weaponDiv = document.querySelector('div.weapon');
-
-const btns = Array.from(weaponDiv.querySelectorAll('button'));
-btns.forEach(btn => btn.addEventListener("click", handleClick));
-
-function handleClick(e) {
-    let computerChoice = getComputerChoice();
-    let playerChoice = e.target.id;
-
-
-    playerChoiceDiv.textContent = `The player chose ${playerChoice} as a weapon!`;
-    computerChoiceDiv.textContent = `The computer counters with ${computerChoice}.`;
-    resultsDiv.textContent = playRound(playerChoice, computerChoice);
-
-}
-
-
-playerScore = 0;
-computerScore = 0;
-
-let scorePlayerNode = document.querySelector('.score-player');
-let scoreComputerNode = document.querySelector('.score-computer');
-let matchResultNode = document.querySelector('.match-result');
-
-function updateScore() {
-    if (playerScore == 5 ) {
-        matchResultNode.textContent = "You win!";
-    }
-
-    else if (computerScore == 5) {
-        matchResultNode.textContent = "Computer wins!"
-    }
-    scorePlayerNode.textContent = `Player score: ${playerScore}`;
-    scoreComputerNode.textContent = `Computer score: ${computerScore}`;
-}
-
